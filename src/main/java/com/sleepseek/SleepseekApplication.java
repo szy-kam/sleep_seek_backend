a62b8b3e17cf80b4fb6668280eed9e22900b980c
Kamil <kamil@pop-os.localdomain>
2020-10-13 20:36:43 +0200

    Initial commit

---
+++ src/main/java/com/sleepseek/SleepseekApplication.java
@@ -0,0 +1,13 @@
+package com.sleepseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepseekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepseekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sleepseek/SleepseekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleepseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepseekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
