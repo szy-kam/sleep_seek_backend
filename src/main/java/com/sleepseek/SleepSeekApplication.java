d027d0b5005429acf9a258f0a24fe01beef1670a
Kamil Domagała <47863877+kadomagala@users.noreply.github.com>
2020-12-13 15:08:48 +0100

    Create SleepSeekApplication.java

---
+++ src/main/java/com/sleepseek/SleepSeekApplication.java
@@ -0,0 +1,13 @@
+package com.sleepseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepSeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepSeekApplication.class, args);
+    }
+
+}
