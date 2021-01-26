d0acd65fa31502e2c3cfa9773f806c3ec4f7c926
Kamil Domagała <47863877+kadomagala@users.noreply.github.com>
2021-01-26 03:26:32 +0100

    AccommodationFacade tests

--- src/test/java/com/sleepseek/accommodation/AccommodationFacadeTest.java
+++ src/test/java/com/sleepseek/accommodation/AccommodationFacadeTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,6 +26,7 @@ class AccommodationFacadeTest {
     private static final Long VALID_SLEEPERS_CAP = 10L;
     private static final Long VALID_PRICE = 10L;
 
+    private List<String> VALID_PROPERTIES = Arrays.asList(AccommodationPropertyDefinition.BATH.getName(), AccommodationPropertyDefinition.COOLING.getName());
 
 
     @Mock
@@ -64,4 +66,268 @@ class AccommodationFacadeTest {
                 ));
     }
 
+    @Test
+    void addAccommodation_nullSleepersCapacityNullQuantityNullPriceNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, null, null, null, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullQuantityNullPriceNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, VALID_SLEEPERS_CAP, null, null, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullSleepersCapacityNullPriceNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, null, VALID_QUANTITY, null, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullSleepersCapacityNullQuantityNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, null, null, VALID_PRICE, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullSleepersCapacityNullQuantityNullPrice_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, null, null, null, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullQuantityNullPriceNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, VALID_SLEEPERS_CAP, null, null, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullSleepersCapacityNullPriceNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, null, VALID_QUANTITY, null, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullSleepersCapacityNullQuantityNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, null, null, VALID_PRICE, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullSleepersCapacityNullQuantityNullPrice_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, null, null, null, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullPriceNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, VALID_SLEEPERS_CAP, VALID_QUANTITY, null, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullQuantityNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, VALID_SLEEPERS_CAP, null, VALID_PRICE, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullQuantityNullPrice_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, VALID_SLEEPERS_CAP, null, null, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullSleepersCapacityNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, null, VALID_QUANTITY, VALID_PRICE, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullSleepersCapacityNullPrice_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, null, VALID_QUANTITY, null, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayIdNullSleepersCapacityNullQuantity_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, null, null, VALID_PRICE, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage(),
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullPriceNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, VALID_SLEEPERS_CAP, VALID_QUANTITY, null, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.PRICE_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullQuantityNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, VALID_SLEEPERS_CAP, null, VALID_PRICE, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullQuantityNullPrice_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, VALID_SLEEPERS_CAP, null, null, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullSleepersCapacityNullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, null, VALID_QUANTITY, VALID_PRICE, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullSleepersCapacityNullPrice_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, null, VALID_QUANTITY, null, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage(),
+                        AccommodationErrorCodes.PRICE_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullProperties_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, VALID_SLEEPERS_CAP, VALID_QUANTITY, VALID_PRICE, null));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.PROPERTIES_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullPrice_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, VALID_SLEEPERS_CAP, VALID_QUANTITY, null, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.PRICE_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullQuantity_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, VALID_SLEEPERS_CAP, null, VALID_PRICE, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.QUANTITY_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullSleepersCap_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(VALID_STAY_ID, null, VALID_QUANTITY, VALID_PRICE, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.SLEEPERS_CAP_NULL.getMessage()
+                ));
+    }
+
+    @Test
+    void addAccommodation_nullStayId_throwsExceptionWithErrorCodes() {
+        var exception = assertThrows(AccommodationValidationException.class, () -> addAccommodation(null, VALID_SLEEPERS_CAP, VALID_QUANTITY, VALID_PRICE, VALID_PROPERTIES));
+        assertAll(
+                () -> assertThat(exception.getMessage()).contains(
+                        AccommodationErrorCodes.STAY_NULL.getMessage()
+                ));
+    }
 }
