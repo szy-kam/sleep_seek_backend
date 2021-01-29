fc04aa769cf84fb6453ad7ead34584f8686deaa5
Kamil Domagała <47863877+kadomagala@users.noreply.github.com>
2021-01-29 18:45:50 +0100

    Update StayRepository.java

--- src/main/java/com/sleepseek/stay/StayRepository.java
+++ src/main/java/com/sleepseek/stay/StayRepository.java
@@ -14,7 +14,7 @@ interface StayRepository extends JpaRepository<Stay, Long> {
             "SELECT * " +
                     "FROM stays s LEFT JOIN users u ON s.user_id = u.id " +
                     "WHERE " +
-                    "(:priceFrom IS NULL OR s.min_price > :priceFrom::bigint) AND (:priceToo IS NULL OR s.min_price < :priceToo::bigint)" +
+                    "(:priceFrom IS NULL OR s.min_price > CAST(:priceFrom AS bigint)) AND (:priceToo IS NULL OR s.min_price < CAST(:priceTo AS bigint))" +
                     "AND (:username IS NULL OR u.username LIKE :username) " +
                     "AND (:category IS NULL OR s.category LIKE :category) " +
                     "AND (:city IS NULL OR s.city LIKE :city) " +
@@ -37,7 +37,7 @@ interface StayRepository extends JpaRepository<Stay, Long> {
             "SELECT * " +
                     "FROM stays s LEFT JOIN users u ON s.user_id = u.id " +
                     "WHERE " +
-                    "(:priceFrom IS NULL OR s.min_price > :priceFrom::bigint) AND (:priceToo IS NULL OR s.min_price < :priceToo::bigint)" +
+                    "(:priceFrom IS NULL OR s.min_price > CAST(:priceFrom AS bigint)) AND (:priceToo IS NULL OR s.min_price < CAST(:priceTo AS bigint))" +
                     "AND (:username IS NULL OR u.username LIKE :username) " +
                     "AND (:category IS NULL OR s.category LIKE :category) " +
                     "AND (:city IS NULL OR s.city LIKE :city) " +
@@ -60,7 +60,7 @@ interface StayRepository extends JpaRepository<Stay, Long> {
             "SELECT * " +
                     "FROM stays s LEFT JOIN users u ON s.user_id = u.id " +
                     "WHERE " +
-                    "(:priceFrom IS NULL OR s.min_price > :priceFrom::bigint) AND (:priceToo IS NULL OR s.min_price < :priceToo::bigint)" +
+                    "(:priceFrom IS NULL OR s.min_price > CAST(:priceFrom AS bigint)) AND (:priceToo IS NULL OR s.min_price < CAST(:priceTo AS bigint))" +
                     "AND (:username IS NULL OR u.username LIKE :username) " +
                     "AND (:category IS NULL OR s.category LIKE :category) " +
                     "AND (:city IS NULL OR s.city LIKE :city) " +
@@ -83,7 +83,7 @@ interface StayRepository extends JpaRepository<Stay, Long> {
             "SELECT * " +
                     "FROM stays s LEFT JOIN users u ON s.user_id = u.id " +
                     "WHERE " +
-                    "(:priceFrom IS NULL OR s.min_price > :priceFrom::bigint) AND (:priceToo IS NULL OR s.min_price < :priceToo::bigint)" +
+                    "(:priceFrom IS NULL OR s.min_price > CAST(:priceFrom AS bigint)) AND (:priceToo IS NULL OR s.min_price < CAST(:priceTo AS bigint))" +
                     "AND (:username IS NULL OR u.username LIKE :username) " +
                     "AND (:category IS NULL OR s.category LIKE :category) " +
                     "AND (:city IS NULL OR s.city LIKE :city) " +
@@ -109,7 +109,7 @@ interface StayRepository extends JpaRepository<Stay, Long> {
                     "ON s.id = res.stay_id " +
                     "LEFT JOIN users u ON u.id = s.user_id " +
                     "WHERE " +
-                    "(:priceFrom IS NULL OR s.min_price > :priceFrom::bigint) AND (:priceToo IS NULL OR s.min_price < :priceToo::bigint)" +
+                    "(:priceFrom IS NULL OR s.min_price > CAST(:priceFrom AS bigint)) AND (:priceToo IS NULL OR s.min_price < CAST(:priceTo AS bigint))" +
                     "AND (:username IS NULL OR u.username LIKE :username) " +
                     "AND (:category IS NULL OR s.category LIKE :category) " +
                     "AND (:city IS NULL OR s.city LIKE :city) " +
@@ -135,7 +135,7 @@ interface StayRepository extends JpaRepository<Stay, Long> {
                     "(SELECT stay_id, AVG(r.rating) avgRate FROM reviews r GROUP BY stay_id) res " +
                     "LEFT JOIN users u ON u.id = s.user_id " +
                     "WHERE " +
-                    "(:priceFrom IS NULL OR s.min_price > :priceFrom::bigint) AND (:priceToo IS NULL OR s.min_price < :priceToo::bigint)" +
+                    "(:priceFrom IS NULL OR s.min_price > CAST(:priceFrom AS bigint)) AND (:priceToo IS NULL OR s.min_price < CAST(:priceTo AS bigint))" +
                     "AND (:username IS NULL OR u.username LIKE :username) " +
                     "AND (:category IS NULL OR s.category LIKE :category) " +
                     "AND (:city IS NULL OR s.city LIKE :city) " +
