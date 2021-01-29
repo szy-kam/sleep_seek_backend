22747b2d5e2a649d5d1755be1a5d5464c2f75b9a
Kamil Domagała <47863877+kadomagala@users.noreply.github.com>
2021-01-29 17:08:17 +0100

    postgres fix

--- src/main/java/com/sleepseek/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/sleepseek/infrastructure/security/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.sleepseek.infrastructure.security;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 class SecurityConfiguration {
@@ -15,7 +18,23 @@ class SecurityConfiguration {
     }
 
     @Bean
-    SecurityConstants securityConstants(){
+    SecurityConstants securityConstants() {
         return new SecurityConstants();
+    }
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 }
